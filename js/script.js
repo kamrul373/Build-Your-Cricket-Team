cb788483168aeae600f8092fd0860f731bf8738b
unknown <textile.zero@gmail.com>
2022-08-22 13:49:28 +0600

    selected five sidebar designed and all areas meaningful comments added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 displayPlayersInGrid("players-area");
 
 
+// test function
+let selected = [];
+function selectedPlayer(playerinfo) {
+
+    const Info = playerinfo;
+    const data = playerinfo.parentNode.children[0].innerText;
+    selected.push(data);
+
+}
