1bc7aae32cda0f3a137272e8914b5167c8c1fbf7
unknown <textile.zero@gmail.com>
2022-08-22 21:58:15 +0600

    players selection function added & max 5 players can be selected & disabling buttong after select a player

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,8 @@
+// display player grids function
+
 function displayPlayersInGrid(containerId) {
     const playerContainer = document.getElementById(containerId);
+    // player info array contains object
     const playersInfo = [
         { name: "Tamim Iqbal", },
         { name: "Mashrafe Bin Mortaza", },
@@ -11,6 +14,14 @@ function displayPlayersInGrid(containerId) {
         { name: "Liton Das", },
         { name: "Mehedi Hasan", },
     ];
+    generatePlayerGrid(playersInfo, playerContainer);
+}
+
+// using for loop to display palyer grid dynamically
+// using innerHtml += features to append new element
+
+function generatePlayerGrid(playersInfo, playerContainer) {
+    // generating dynamic player grid 
     for (let i = 0; i < playersInfo.length; i++) {
         playerContainer.innerHTML += `
         <div class="player-card text-center text-white bg-black shadow-xl">
@@ -19,9 +30,38 @@ function displayPlayersInGrid(containerId) {
         </div>
         <div class="player-info my-4">
             <h2 class="font-semibold my-4">${playersInfo[i].name}</h2>
-            <button id="player-${i + 1}-btn" class="bg-blue-600 px-16 py-1" onclick="selectedPlayer(this)">Select</button>
+            <button id="player-${i + 1}-btn" class="px-16 py-1 bg-[#015196]" onclick="displaySelectedPlayers(this)" type="submit">Select</button>
         </div>
      </div>`
     }
-
 }
+// selected player function 
+// using createElement and appending JS functions
+
+function displaySelectedPlayers(playerinfo) {
+    // getting initial info
+    const info = playerinfo;
+    const selectedPlayerContainer = document.getElementById("selected-players");
+    const data = info.parentNode.children[0].innerText;
+
+    const selectedPlayersNodeLength = document.querySelectorAll("#selected-players li").length;
+
+    // validation for max 5 player can be added
+    if (selectedPlayersNodeLength > 4) {
+        return alert("Maximum 5 plyaers are allowed to add in your selection panel");
+    }
+    // disabling current button 
+    info.setAttribute("disabled", true);
+    info.style.background = "gray";
+
+    // creating new element
+    const li = document.createElement("li");
+    li.innerText = data
+    li.classList.add("text-white");
+    li.classList.add("my-2");
+
+    // appending new element to its parent <ul>
+    selectedPlayerContainer.append(li);
+}
+
+
--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 // showing players 
 displayPlayersInGrid("players-area");
 
+// all player expense calculation 
+document.getElementById("calculate").addEventListener("click", function (event) {
+    let numberOfSelectedPlayers = getNodeLength("#selected-players li");
+    numberOfSelectedPlayers = parseInt(numberOfSelectedPlayers);
+    const singlePlayerExpense = getInputValueById("single-player-cost");
+    // validation
+    if (numberOfSelectedPlayers === 0) {
+        return alert("Please first select some players to calculate expense");
+    }
+    else if (Math.sign(singlePlayerExpense) == -1) {
+        return alert("You cant put negative value");
+    } else if (isNaN(singlePlayerExpense)) {
+        return alert("Please put a valid expense for a player");
+    }
+    // total expense of players
+    const playersTotalExpense = gettotalExpenseOfPlayers(numberOfSelectedPlayers, singlePlayerExpense);
 
-// test function
-let selected = [];
-function selectedPlayer(playerinfo) {
-
-    const Info = playerinfo;
-    const data = playerinfo.parentNode.children[0].innerText;
-    selected.push(data);
-
-}
+    // setting players expense in frontend
+    setInnerText("players-expense", playersTotalExpense);
+});
